bbf760d756728618ac29e84d711afd168d46bd5d
Armand Mosnier <armosnie@f4r1s16.clusters.42paris.fr>
2025-08-22 12:34:02 +0200

    bug : deadlock

--- src/routine.c
+++ src/routine.c
@@ -6,7 +6,7 @@
 /*   By: armosnie <armosnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/19 13:14:59 by armosnie          #+#    #+#             */
-/*   Updated: 2025/08/22 12:09:25 by armosnie         ###   ########.fr       */
+/*   Updated: 2025/08/22 12:32:28 by armosnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,10 @@ void	take_forks(t_philo *philo)
 void	eat(t_philo *philo)
 {
 	if (simulation_done(philo) == 1)
+	{
+		drop_forks(philo);
 		return ;
+	}
 	print_status(philo, "is eating...", "\033[36m");
 	pthread_mutex_lock(&philo->meal_time);
 	philo->last_meal_time = get_current_time(philo->data);
