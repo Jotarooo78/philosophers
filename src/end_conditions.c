9cb595aea2dad6493f67a4e2e3be0951a4f50745
Armand Mosnier <armosnie@f4r3s13.clusters.42paris.fr>
2025-08-25 13:45:39 +0200

    debug version

--- src/end_conditions.c
+++ src/end_conditions.c
@@ -6,7 +6,7 @@
 /*   By: armosnie <armosnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 16:30:47 by armosnie          #+#    #+#             */
-/*   Updated: 2025/08/25 12:36:09 by armosnie         ###   ########.fr       */
+/*   Updated: 2025/08/25 13:43:56 by armosnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,49 +56,77 @@ int	all_ate_enough(t_philo *philo)
 
 void	check_is_over(t_data *data, char **argv)
 {
-	int	i;
-	
+	int		i;
+	int		loop_count;
+	long	last_print;
+	long	current_time;
+	long	time_since_meal;
+
+	loop_count = 0;
+	last_print = 0;
+	if (data->nb_philos == 1)
+	{
+		while (get_current_time(data) < data->time_to_die)
+			usleep(1000);
+		print_status(data->philos, "died", "\033[31m");
+		pthread_mutex_lock(&data->death_mutex);
+		data->is_over = 1;
+		pthread_mutex_unlock(&data->death_mutex);
+		return ;
+	}
+	usleep(5000);
 	while (1)
 	{
 		i = 0;
+		current_time = get_current_time(data);
+		// Debug toutes les 100ms
+		if (current_time - last_print >= 100)
+		{
+			printf("[MONITOR] Time: %ld, Checking %d philosophers\n",
+				current_time, data->nb_philos);
+			last_print = current_time;
+		}
 		while (i < data->nb_philos)
 		{
-			// DEBUG: while
-            // DEBUG: Afficher les temps pour comprendre
-            pthread_mutex_lock(&data->philos[i].meal_time);
-            long time_since_meal = get_time_last_meal(&data->philos[i]);
-            long current = get_current_time(data);
-            
-            // Si un philosophe approche de la mort, debugger
-            if (time_since_meal > data->time_to_die - 100)
-            {
-                printf("[DEBUG] Philo %d: current=%ld, last_meal=%ld, since_meal=%ld, limit=%ld\n",
-                       data->philos[i].id, current, data->philos[i].last_meal_time,
-                       time_since_meal, data->time_to_die);
-            }
-            pthread_mutex_unlock(&data->philos[i].meal_time);
-			if (death_by_starvation(data, i) != 0)
-                return ;
+			pthread_mutex_lock(&data->philos[i].meal_time);
+			time_since_meal = get_time_last_meal(&data->philos[i]);
+			// Si proche de la mort, afficher plus de détails
+			if (time_since_meal >= data->time_to_die - 5)
+			{
+				printf("[CRITICAL] Philo %d: time_since_meal=%ld (limit=%ld)\n",
+					data->philos[i].id, time_since_meal, data->time_to_die);
+			}
+			if (time_since_meal >= data->time_to_die)
+			{
+				pthread_mutex_unlock(&data->philos[i].meal_time);
+				print_status(&data->philos[i], "died", "\033[31m");
+				pthread_mutex_lock(&data->death_mutex);
+				data->is_over = 1;
+				pthread_mutex_unlock(&data->death_mutex);
+				return ;
+			}
+			pthread_mutex_unlock(&data->philos[i].meal_time);
 			i++;
 		}
-		if (argv[5])
-		{
-			if (all_ate_enough(data->philos) != 0)
-				return ;
-		}
+		if (argv[5] && all_ate_enough(data->philos) != 0)
+			return ;
+		loop_count++;
+		usleep(1000); // Vérifier toutes les 1ms
 	}
 }
-			// DEBUG: while
-            // // DEBUG: Afficher les temps pour comprendre
-            // pthread_mutex_lock(&data->philos[i].meal_time);
-            // long time_since_meal = get_time_last_meal(&data->philos[i]);
-            // long current = get_current_time(data);
-            
-            // // Si un philosophe approche de la mort, debugger
-            // if (time_since_meal > data->time_to_die - 100)
-            // {
-            //     printf("[DEBUG] Philo %d: current=%ld, last_meal=%ld, since_meal=%ld, limit=%ld\n",
-            //            data->philos[i].id, current, data->philos[i].last_meal_time,
-            //            time_since_meal, data->time_to_die);
-            // }
-            // pthread_mutex_unlock(&data->philos[i].meal_time);
+
+// DEBUG: while
+// // DEBUG: Afficher les temps pour comprendre
+// pthread_mutex_lock(&data->philos[i].meal_time);
+// long time_since_meal = get_time_last_meal(&data->philos[i]);
+// long current = get_current_time(data);
+
+// // Si un philosophe approche de la mort, debugger
+// if (time_since_meal > data->time_to_die - 100)
+// {
+//     printf("[DEBUG] Philo %d: current=%ld, last_meal=%ld, since_meal=%ld,
+	// limit=%ld\n",
+//            data->philos[i].id, current, data->philos[i].last_meal_time,
+//            time_since_meal, data->time_to_die);
+// }
+// pthread_mutex_unlock(&data->philos[i].meal_time);
